c9e5af34e65543caaa3fdb04be7d9d1df3f13478
Muh Fauzi <igscrut@gmail.com>
2026-09-02 21:52:48 +0800

    day1.java
    Brotosaurus dan Gigantosaurus

---
+++ day1.java
@@ -0,0 +1,18 @@
+public class day1{
+
+  public static void main(String [] args){
+
+    System.out.println("Hello Word");
+
+    // Brontosaurus dan Gigantosaurus
+
+    /* Brontosaurus dan Gigantosaurus sedang menikmati matahari terbit di atas gunung Jaya Wijaya
+       Brontosaurus dan Gigantosaurus yang sedang duduk santai. tiba tiba Brotosaurus melihat cahaya
+       kemerahan yang jatuh dari langit, Brontosaurus menunjuk langit.
+
+    Tamat
+           
+    */
+  }
+
+}
