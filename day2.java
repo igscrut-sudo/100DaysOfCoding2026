ce6451df200381e8c04f179b4b54fb9d7847972d
Muh Fauzi <igscrut@gmail.com>
2026-09-03 21:56:15 +0800

    day2.java

---
+++ day2.java
@@ -0,0 +1,12 @@
+public class day2 {
+
+    public static void main(String[] args) {
+
+        String text = "Mencetak teks denga format khusus";
+
+        System.out.println("println = mencetak teks dengan bari baru");
+        System.out.print("print = Mencetak tanpa baris baru    ");
+        System.out.printf("printf = %s",text);
+    }
+    
+}
