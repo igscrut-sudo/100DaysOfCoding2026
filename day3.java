56de1c527f03a40ee51f4b923396d24c199f8741
Muh Fauzi <igscrut@gmail.com>
2026-09-04 22:53:15 +0800

    day3.java

---
+++ day3.java
@@ -0,0 +1,8 @@
+public class day3 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Nama:\tMuh Fauzi\nNIM:\tD0225301\nAlamat:\t\"Sapokayyang\"\nHuruf Vavorit:\t\'A\'");
+       
+    } 
+}
