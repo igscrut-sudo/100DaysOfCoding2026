df55c1df878f3d77a650f82e7c0ba77d9b0ccaca
Muh Fauzi <igscrut@gmail.com>
2026-09-05 11:31:16 +0800

    day4.java

---
+++ day4.java
@@ -0,0 +1,14 @@
+public class day4 {
+
+    public static void main(String[] args) {
+        // Tipe data Numerik Bilangan Bulat
+
+        byte a = 12;
+        short b = 4;
+        int c = 10;
+        long d = 12456;
+
+        System.out.printf("Byte: %d\nShort: %d\nInt: %d\nLong: %d",a,b,c,d);
+    }
+    
+}
