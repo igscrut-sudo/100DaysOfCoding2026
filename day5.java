1e5c6d942bedaa3c24589343990b642afe081367
Muh Fauzi <igscrut@gmail.com>
2026-09-06 20:17:18 +0800

    day5.java

---
+++ day5.java
@@ -0,0 +1,11 @@
+public class day5 {
+
+    public static void main(String[] args) {
+        
+        double a = 12.9;
+        float b = 12;
+
+        System.err.printf("Double: %f\nFloat: %f",a,b);
+    }
+    
+}
