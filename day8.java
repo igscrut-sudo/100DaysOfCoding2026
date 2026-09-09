958bf30c9c81ce7f0faa41c8c4a1663cad99764a
Muh Fauzi <igscrut@gmail.com>
2026-09-09 23:14:29 +0800

    day8.java

---
+++ day8.java
@@ -0,0 +1,25 @@
+public class day8 {
+
+    public static void main(String[] args) {
+        
+        String nama;
+        int angkatan;
+        char kelas;
+        double ipk;
+
+       
+        nama ="Muhammad Fauzi Al-Zahmi";
+        angkatan=2025;
+        kelas = 'D';
+        ipk = 3.99;
+
+
+        System.out.printf("Nama: %s \nAngkatan: %d\nKelas %s\nipk: %.2f", nama,angkatan,kelas,ipk);
+       
+
+ 
+
+    }
+}
+    
+
