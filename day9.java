2f89f8839173e1270af2d970b309c6d649db11f5
Muh Fauzi <igscrut@gmail.com>
2026-09-10 23:20:21 +0800

    day9.java

---
+++ day9.java
@@ -0,0 +1,12 @@
+public class day9 {
+
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        double r = 7;
+        double luas = PI * r * r;
+
+        System.out.println("Nilai PI = " + PI);
+        System.out.println("Jari-jari = " + PI);
+        System.out.println("Luas lingkaran = " + luas);
+    }
+}
