93094e2715f22d0b9f85055047b3d0d0ddf8ec44
Muh Fauzi <igscrut@gmail.com>
2026-09-12 00:53:49 +0800

    day10.java

---
+++ day10.java
@@ -0,0 +1,16 @@
+public class day10 {
+    public static void main(String[] args) {
+     
+
+        // Mengakses batas minimal dan maksimal menggunakan Wrapper Class
+        System.out.println("--- Batas Nilai Integer Java ---");
+        System.out.println("Byte   : " + Byte.MIN_VALUE + " s.d. " + Byte.MAX_VALUE);
+        System.out.println("Short  : " + Short.MIN_VALUE + " s.d. " + Short.MAX_VALUE);
+        System.out.println("Integer: " + Integer.MIN_VALUE + " s.d. " + Integer.MAX_VALUE);
+        System.out.println("Long   : " + Long.MIN_VALUE + " s.d. " + Long.MAX_VALUE);
+
+    
+        
+    }
+}
+    
