ebee824346fa18c5155817a7b1e9e792d955fa96
Muh Fauzi <igscrut@gmail.com>
2026-09-12 09:53:56 +0800

    day11.java

---
+++ day11.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class day11 {
+
+    public static void main(String[] args) {
+
+        Scanner masuk = new Scanner (System.in);
+
+        System.out.print("Admin: ");
+        String nama = masuk.nextLine();
+
+        System.out.println("===== Output =====");
+        System.out.println(nama);
+    }
+    
+}
