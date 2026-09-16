449e2b5dac90770459d7aee45ffd1af5da1a4e64
Muh Fauzi <igscrut@gmail.com>
2026-09-16 23:19:16 +0800

    day15.java

---
+++ day15.java
@@ -0,0 +1,15 @@
+public class day15 {
+
+    public static void main(String[] args) {
+        
+        int a = 37;
+        int b = 45;
+
+        int d = a * b;
+        int c = a / b;
+
+        System.out.println(d);
+        System.out.println(c);
+    }
+    
+}
