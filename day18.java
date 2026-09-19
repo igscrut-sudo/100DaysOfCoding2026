2c9c91af48a7fb3b58cbb520a50432eb9776c149
Muh Fauzi <igscrut@gmail.com>
2026-09-19 15:40:26 +0800

    day18.java

---
+++ day18.java
@@ -0,0 +1,12 @@
+public class day18 {
+    public static void main(String[] args) {
+        // Tipe data kecil (int berukuran 4 bytes)
+        int tipeKecil = 100;
+        
+        // Konversi otomatis ke tipe data besar (double berukuran 8 bytes)
+        double tipeBesar = tipeKecil;
+        
+        System.out.println("Nilai Integer (Kecil) : " + tipeKecil);
+        System.out.println("Nilai Double (Besar)  : " + tipeBesar);
+    }
+}
