632b47b192afd2ca79e234224981b5c0be049fee
Muh Fauzi <igscrut@gmail.com>
2026-09-20 22:01:02 +0800

    day19.java

---
+++ day19.java
@@ -0,0 +1,13 @@
+public class day19 {
+
+    public static void main(String[] args) {
+        
+        double nb = 140.000;
+
+        int ni = (int) nb;
+
+        System.out.println(nb+" sebelum dikonversi paksa");
+        System.out.println(ni+"sesudah dikonversi paksa");
+    }
+    
+}
