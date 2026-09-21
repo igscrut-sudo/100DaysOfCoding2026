8360ea7c883223f55f8b49d1499bc1255f6dd643
Muh Fauzi <igscrut@gmail.com>
2026-09-21 23:34:26 +0800

    day20.java

---
+++ day20.java
@@ -0,0 +1,16 @@
+public class day20 {
+
+    public static void main(String[] args) {
+        
+        int nim = 301;
+
+
+        String nim1 = int.tostring(nim);
+
+        System.out.println("Hasil konversi int menggunakan tostring"+nim1);
+
+
+        
+    }
+    
+}
