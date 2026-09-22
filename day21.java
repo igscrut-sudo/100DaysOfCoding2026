6590a367006855030e91824866ec0ad044ef8c58
Muh Fauzi <igscrut@gmail.com>
2026-09-22 22:00:48 +0800

    day21.java

---
+++ day21.java
@@ -0,0 +1,15 @@
+public class day21 {
+
+    public static void main(String[] args) {
+        
+        String angka = "123";
+
+        
+        int a = Integer.parseInt(angka);
+
+        System.out.println(""+a);
+
+
+    }
+    
+}
