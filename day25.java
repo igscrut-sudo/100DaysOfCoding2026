b7eb518ec2463cf87e1d4803610141f69e7c5ae4
Muh Fauzi <igscrut@gmail.com>
2026-09-26 21:51:14 +0800

    day25.java

---
+++ day25.java
@@ -0,0 +1,18 @@
+public class day25 {
+
+    public static void main(String[] args) {
+
+    final double pi = 3.14;
+    double r = 12;
+
+
+
+     double  l = pi * r *r;
+
+    System.out.println(l);
+        
+    
+        
+    }
+    
+}
